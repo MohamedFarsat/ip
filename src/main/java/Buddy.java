a9c2d3ed130212849e376d4dd4ff1c37a9b93667
MohamedFarsat <farsatlsp@gmail.com>
2026-08-16 01:46:12 +0800

    Changed name and added greeting + bye

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,18 @@
+public class Buddy {
+    public static final String name = "Buddy";
+    public static void main(String[] args) {
+        String banner = " ____            _     _       \n"
+                + "| __ ) _   _  __| | __| |_   _ \n"
+                + "|  _ \\| | | |/ _` |/ _` | | | |\n"
+                + "| |_) | |_| | (_| | (_| | |_| |\n"
+                + "|____/ \\__,_|\\__,_|\\__,_|\\__, |\n"
+                + "                          |___/ \n";
+        System.out.println("---------------------------------------------------------------");
+        System.out.println(banner);
+        System.out.println("Hello! I'm "+ name);
+        System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------------------------------------------");
+    }
+}
