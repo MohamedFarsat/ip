699e4cdb4b208d3df97daef3fbd6c83f87a78dfb
MohamedFarsat <farsatlsp@gmail.com>
2026-08-30 13:52:09 +0800

    Add Level 1 echo command loop
    Read user commands continuously, echo non-bye input, and exit cleanly when the user enters bye.

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Entry point for Buddy, a simple chatbot used for the iP.
  */
 public class Buddy {
     public static final String NAME = "Buddy";
+    private static final String LINE = "---------------------------------------------------------------";
 
     /**
      * Greets the user and exits.
@@ -16,12 +19,24 @@ public class Buddy {
                 + "| |_) | |_| | (_| | (_| | |_| |\n"
                 + "|____/ \\__,_|\\__,_|\\__,_|\\__, |\n"
                 + "                          |___/ \n";
-        System.out.println("---------------------------------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(LINE);
         System.out.println(banner);
         System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("---------------------------------------------------------------");
+        System.out.println(LINE);
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(LINE);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(LINE);
+        }
     }
 }
